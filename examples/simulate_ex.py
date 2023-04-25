- Fixed broadcasting of y0 in Interaction.master.

examples/simulate_ex.py
@@ -63,7 +63,7 @@ def example(n: Union[set, int] = None):
 
         ca40 = sim.Atom(states=s + p, decay_map=decay)  # The Atom with all states and the decay information.
 
-        pol = sim.Polarization([0, 1, 1], q_axis=[0, 1, 1], vec_as_q=True)
+        pol = sim.Polarization([0, 1, 0], q_axis=2, vec_as_q=True)
         laser_sp = sim.Laser(freq=f_sp, polarization=pol, intensity=500)  # Linear polarized laser for the ground-state
         # transition with 500 uW / mm**2.
 
@@ -75,8 +75,10 @@ def example(n: Union[set, int] = None):
 
         times = np.linspace(0.1, 0.5, 1001, dtype=float)
         results = inter.rates(times, delta=np.linspace(-50, 50, 101))
+        i = ca40.get_state_indexes('p')
         for res in results:
-            plt.plot(times, res[-1])
+            plt.plot(times, res[-2])
+            # plt.plot(times, np.sum(res[i], axis=0))
         plt.show()
 
         # inter.rates(t=t)  # Solve the rate equations for t, assuming equal population in all s-states.

pycol/_simulate.py
@@ -849,10 +849,12 @@ def _cast_y0(y0: Optional[array_like], i_solver: int, atom: Atom):
             y0[gs] = 1 / gs.size
             return np.diag(y0), c_complex_p
         y0 = np.array(y0, dtype=complex, order='C')
-        if not y0.shape or y0.shape[-1] != size:
-            raise ValueError('\'y0\' must have size {} in the last axis but has shape {}.'.format(size, y0.shape))
+        if not y0.shape or (len(y0.shape) == 1 and y0.shape[-1] != size) \
+                or (len(y0.shape) > 1 and y0.shape[-2:] != (size, size)):
+            raise ValueError('\'y0\' must have a total shape of {}, or shape {} in the last two axes but has shape {}.'
+                             .format((size, ), (size, size), y0.shape))
         if len(y0.shape) > 1:
-            y0 /= np.expand_dims(np.sum(np.diagonal(y0, axis1=-2, axis2=-1)), axis=(-1, -1))
+            y0 /= np.sum(np.diagonal(y0, axis1=-2, axis2=-1), axis=-1)[:, None, None]
         else:
             y0 = np.diag(y0 / np.sum(y0))
         return y0, c_complex_p
@@ -1018,6 +1020,8 @@ class Interaction:
         :param value: The new lasers of the interaction.
         :returns: None.
         """
+        if value is None:
+            value = []
         self._lasers = list(value)
         dll.interaction_clear_lasers(self.instance)
         for laser in self.lasers:
@@ -1254,7 +1258,8 @@ class Interaction:
         :param v: Atom velocities. Must be a scalar or have shape (n, ) or (n, 3). In the first two cases,
          the velocity vector(s) are assumed to be aligned with the x-axis.
         :param y0: The initial state / density matrix of the atom.
-         This must be None or have shape (n, [#states], #states). If None, the ground states are populated equally.
+         This must be None or have shape (#states, ) or (n, #states, #states).
+         If None, the ground states are populated equally.
         :returns: The integrated master equation as a complex-valued array of shape (n, #states, #states, #times).
         """
         t = np.asarray(t, dtype=float).flatten()