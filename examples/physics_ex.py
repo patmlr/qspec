0956dcd3c68efc176b6840bfd4647aa1c1c4d304
Patrick Müller <97469533+patmlr@users.noreply.github.com>
2024-10-29 17:32:50 -0700

    - physics: Non-perturbative Zeeman shift (full diagonalization) are now sorted according to their m and F quantum numbers and a list of the F and m quantum numbers is also returned.

--- examples/physics_ex.py
+++ examples/physics_ex.py
@@ -203,18 +203,21 @@ def example(n=None):
         plt.show()
 
     if 5 in n:
-        i = 0.5
+        i = 4
         j = 4
         mu = -0.2310
         g_n = mu / i
         g_j = -1.25
         a_hyper = -1039.125
         b = np.linspace(0., 0.4, 401)
-        e_eig, m_list = qs.hyper_zeeman_num(i, j, None, None, g_n, g_j, a_hyper, 0., b)
+        e_eig, fm_list, m_list = qs.hyper_zeeman_num(i, j, None, None, g_n, g_j, a_hyper, 0., b)
 
-        for im, (_e_eig, _m) in enumerate(zip(e_eig, m_list)):
+        f_plotted = set()
+        for im, (_e_eig, _f_list, _m) in enumerate(zip(e_eig, fm_list, m_list)):
             for k in range(_e_eig.shape[1]):
-                plt.plot(b, _e_eig[:, k], f'-C{im}', label=rf'$m = {_m}$' if k == 0  else None)
+                c = int(_f_list[k] - abs(i - j))
+                plt.plot(b, _e_eig[:, k], f'-C{c}', label=rf'$(F, m) = {_f_list[k], _m}$' if c not in f_plotted else None)  #  if k == 0  else None
+                f_plotted.add(c)
 
         plt.xlabel('Magnetic field (T)')
         plt.ylabel('Frequency shift (MHz)')
--- qspec/physics.py
+++ qspec/physics.py
@@ -679,7 +679,14 @@ def hyper_zeeman_num(i: float, j: float, f: float, m: float, g_n: float, g_j: fl
     e_eig = [_h_eig[0] for _h_eig in h_eig]
     v_eig = [_h_eig[1] for _h_eig in h_eig]
     print([_e_eig.shape for _e_eig in e_eig])
-    return e_eig, m_list
+
+    e_0 = [np.array([hyperfine(i, j, _f, a_hyper, b_hyper) for _f in _f_list], dtype=float) for _f_list in fm_list]
+    inv_order_fm = [list(np.argsort(_e_0)) for _e_0 in e_0]
+    order_fm = [np.array([_inv_order.index(i) for i in range(len(_inv_order))], dtype=int)
+                for _inv_order in inv_order_fm]
+    e_eig = [_e_eig[:, _order] for _e_eig, _order in zip(e_eig, order_fm)]
+
+    return e_eig, fm_list, m_list
 
 
 
