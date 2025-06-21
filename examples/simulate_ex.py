- simulate:   * Added m1 transitions.   * Added an analytical solution to the rate equations. - physics:   * Added a calculation of the Doppler cooling limit.   * Fixed an error in the differential spin-1/2 Zeeman shift.

examples/simulate_ex.py
@@ -271,7 +271,7 @@ def example(n=None):
         inter.controlled = True
         inter.resonance_info()  # Print the resonance info.
 
-        t = np.concatenate([np.zeros(1), np.logspace(-4, 1, 5000)], axis=0)  # Integration time in us.
+        t = np.concatenate([np.zeros(1), np.logspace(-4, 2.5, 5000)], axis=0)  # Integration time in us.
         y0 = li7.get_y0(['s3', 's5'])
 
         y = inter.rates(t, y0=y0)  # Solve the rate equation for t and plot with logarithmic scaling.
@@ -283,7 +283,7 @@ def example(n=None):
         plt.legend()
         plt.show()
 
-        ''' The result is quiet different with fully coherent dynamics. '''
+        ''' The result is quite different with fully coherent dynamics. '''
         y = inter.master(t, y0=y0)  # Solve the master equation for t and plot with logarithmic scaling.
         y = np.diagonal(y[0], axis1=0, axis2=1).real
         plt.xscale('log')
@@ -486,8 +486,8 @@ def example(n=None):
         a31 = 4.857e-5
         print(f'tau: {1e-3 / a31} ms')
 
-        s1 = sim.gen_electronic_state(0., 0, 0, parity='even', ls=(0, 0), label='s1')
-        s3 = sim.gen_electronic_state(f31, 1, 0, parity='even', ls=(0, 1), label='s3')
+        s1 = sim.gen_electronic_state(0., 0, 0, parity='even', ls=[0, 0], label='s1')
+        s3 = sim.gen_electronic_state(f31, 1, 0, parity='even', ls=[0, 1], label='s3')
 
         decay_map = sim.DecayMap(labels=[('s1', 's3')], a=[a31])
 
@@ -518,7 +518,6 @@ def example(n=None):
         plt.xlabel('Time (ms)')
         plt.ylabel('Population')
         plt.show()
-
 
 
 if __name__ == '__main__':

qspec/physics.py
@@ -24,7 +24,7 @@ __all__ = ['L_LABEL', 'E_NORM', 'pi', 'LEMNISCATE', 'mu_N', 'mu_B', 'g_s', 'me_u
            'f_recoil_v', 'get_f', 'get_m', 'hyperfine', 'lande_n', 'lande_j', 'lande_jj', 'g_j', 'lande_f',
            'zeeman_linear', 'hyper_zeeman_linear',
            'hyper_zeeman_ij', 'hyper_zeeman_num', 'hyper_zeeman_12', 'hyper_zeeman_12_d', 'a_hyper_mu', 'a_einstein_m1',
-           'saturation_intensity', 'saturation', 'rabi', 'scattering_rate', 'mass_factor',
+           'temperature_doppler', 'saturation_intensity', 'saturation', 'rabi', 'scattering_rate', 'mass_factor',
            'delta_r2', 'delta_r4', 'delta_r6', 'lambda_r', 'lambda_rn', 'schmidt_line', 'sellmeier',
            'gamma_3d', 'boost', 'doppler_3d', 'gaussian_beam_3d', 'gaussian_doppler_3d', 't_xi', 'thermal_v_pdf',
            'thermal_v_rvs', 'thermal_e_pdf', 'thermal_e_rvs', 'convolved_boltzmann_norm_pdf',
@@ -786,7 +786,6 @@ def lande_f(i: quant_like, j: quant_like, f: quant_like, gi: array_like, gj: arr
     return val
 
 
-
 def hyperfine(i: quant_like, j: quant_like, f: quant_like,
               a_hyper: array_like = 0., b_hyper: array_like = 0., c_hyper: array_like = 0.) -> ndarray:
     r"""
@@ -1089,9 +1088,9 @@ def hyper_zeeman_12_d(j: quant_like, m: quant_like, a_hyper: array_like = 0.,
     x_b0 = a_hyper * (j + 0.5)
 
     _x = b_field * (mu_B * g_j - mu_N * g_i) / x_b0
-    _dx = (mu_B * g_j - mu_N * g_i) / x_b0
+    _dx = (mu_B * g_j - mu_N * g_i) / x_b0 * z_unit
 
-    dx = -mu_B * g_j * m
+    dx = -mu_B * g_j * m * z_unit
 
     if m == j + 0.5:
         x0 = dx + 0.5 * x_b0 * _dx
@@ -1163,6 +1162,20 @@ def a_einstein_m1(f: array_like, j_l: quant_like = 0, j_u: quant_like = 0, ls: q
     mu = (mu * mu_B) ** 2 / (2 * j_u + 1)
 
     return 8 * mu * np.pi ** 2 * sc.mu_0 * f ** 3 / (3 * sc.hbar * sc.c ** 3) * 1e12
+
+
+def temperature_doppler(a):
+    r"""
+    The minimum temperature achievable through Doppler cooling (Doppler limit)
+
+    $$
+    T\frac{\hbar \Gamma}{2k_\mathrm{B}}.
+    $$
+
+    :param a: The inverse lifetime $\Gamma = 1 / \tau$ of the upper state of the cooling transition (MHz).
+    :returns: The temperature $T_\mathrm{Doppler}$ (K).
+    """
+    return 0.5 * sc.hbar * a * 1e6 / sc.k
 
 
 def saturation_intensity(f: array_like, a: array_like, a_dipole: array_like = 1.) -> ndarray:

qspec/simulate/_simulate_cpp.py
@@ -328,9 +328,10 @@ class State:
                 label = '{}({})'.format(int(np.around(freq_j, decimals=0)), j)
             self.instance = dll.state_construct()
             if isinstance(parity, str):
-                if parity not in {'even', 'odd'}:
-                    raise ValueError('Parameter \'parity\' must be either \'even\' (False) or \'odd\' (True).')
-                parity = True if parity == 'odd' else False
+                if parity not in {'even', 'e', 'odd', 'o'}:
+                    raise ValueError('Parameter \'parity\' must be either in {\'even\', \'e\', False}'
+                                     ' or in {\'odd\', \'o\', True}.')
+                parity = True if parity in {'odd', 'o'} else False
             else:
                 parity = bool(parity)
             dll.state_init(self.instance, c_double(freq_j), c_double(j), c_double(i),
@@ -1552,7 +1553,7 @@ class Interaction:
         return results
 
     def rates(self, t: array_like, delta: array_like = None, m: Optional[int] = 0, v: array_like = None,
-              y0: array_like = None):
+              y0: array_like = None, analytic: bool = False):
         """
         Solver for the rate equations. Solutions for n samples can be calculated in parallel.
 
@@ -1564,6 +1565,8 @@ class Interaction:
          the velocity vector(s) is(are) assumed to be aligned with the x-axis.
         :param y0: The initial state of the atom. This must be None or have shape (#states, ) or (n, #states).
          If None, the ground states are populated equally.
+        :param analytic: Calculate the rate equations analytically through a matrix exponential (True)
+         or numerically (False, default).
         :returns: The integrated rate equations as a real-valued array of shape (n, #states, #times).
         """
         t, t_size, ex = _cast_t(t)
@@ -1591,7 +1594,8 @@ class Interaction:
         results = np.zeros((sample_size, self.atom.size, t_size), dtype=float)
         dll.interaction_rates(self.instance, t.ctypes.data_as(c_double_p), delta.ctypes.data_as(c_double_p),
                               v.ctypes.data_as(c_double_p), y0.ctypes.data_as(c_double_p),
-                              results.ctypes.data_as(c_double_p), c_size_t(t_size), c_size_t(sample_size))
+                              results.ctypes.data_as(c_double_p), c_size_t(t_size), c_size_t(sample_size),
+                              c_bool(analytic))
         if ex:
             results = results[:, :, 1:]
         return results

qspec/stats.py
@@ -217,9 +217,9 @@ def median(a: array_like, axis: int = None) -> (ndarray, ndarray, ndarray):
     :returns: The median (0.5-percentile) as well as the left- (~0.1587) and right-sided (~0.8413) 1-sigma percentile
      of a given sample 'a' along the specified 'axis'.
     """
-    med = np.median(a, axis=axis)
-    neg = np.percentile(a, 15.8655254, axis=axis)
-    pos = np.percentile(a, 84.1344746, axis=axis)
+    med = np.nanmedian(a, axis=axis)
+    neg = np.nanpercentile(a, 15.8655254, axis=axis)
+    pos = np.nanpercentile(a, 84.1344746, axis=axis)
     return med, neg, pos
 
 